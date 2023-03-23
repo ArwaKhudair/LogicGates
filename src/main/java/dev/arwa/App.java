e41a7dd6d8f90b451522bb436bf14757c64d209b
Khader-1 <khader2020kh@gmail.com>
2023-03-23 12:15:27 +0200

    initial commit

---
+++ src/main/java/dev/arwa/App.java
@@ -0,0 +1,103 @@
+package dev.arwa;
+
+import java.util.Scanner;
+
+/**
+ * @author Arwa
+ * @version 1.0
+ * @since 2023-03-23
+ *
+ */
+// This is a program that performs logical operations
+// and prints the result truth table in the console
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Logical Operations Program");
+        System.out.println("========================================");
+        int operator = pickOperator();
+        System.out.println("You picked " + operatorChar(operator) + " - " + description(operator));
+        System.out.println("========================================");
+        System.out.println("Truth Table");
+        System.out.println("========================================");
+        System.out.println("A\t" + operatorChar(operator) + "\tB\tResult");
+        System.out.println("========================================");
+        System.out.println("F\t" + operatorChar(operator) + "\tF\t" + performOperation(false, operator, false));
+        System.out.println("F\t" + operatorChar(operator) + "\tT\t" + performOperation(false, operator, true));
+        System.out.println("T\t" + operatorChar(operator) + "\tF\t" + performOperation(true, operator, false));
+        System.out.println("T\t" + operatorChar(operator) + "\tT\t" + performOperation(true, operator, true));
+        System.out.println("========================================");
+        
+    }
+
+    public static int pickOperator() {
+        System.out.println("Pick an operator:");
+        System.out.println("1. AND");
+        System.out.println("2. OR");
+        System.out.println("3. XOR");
+        System.out.println("4. Implication");
+        System.out.println("5. Biconditional");
+
+        Scanner scanner = new Scanner(System.in);
+        int res = scanner.nextInt();
+        scanner.close();
+        return res;
+    }
+
+    public static boolean performOperation(boolean a, int operator, boolean b) {
+        boolean result = false;
+        switch (operator) {
+            case 1:
+                result = a && b;
+                break;
+            case 2:
+                result = a || b;
+                break;
+            case 3:
+                result = a ^ b;
+                break;
+            case 4:
+                result = !a || b;
+            case 5:
+                result = (!a || b) && (a || !b);
+                break;
+            default:
+                System.out.println("Invalid operator");
+                break;
+        }
+        return result;
+    }
+
+    public static String operatorChar(int operator) {
+        switch (operator) {
+            case 1:
+                return "^";
+            case 2:
+                return "v";
+            case 3:
+                return "XOR";
+            case 4:
+                return "->";
+            case 5:
+                return "<->";
+            default:
+                return "Invalid operator";
+        }
+    }
+
+    public static String description(int operator) {
+        switch (operator) {
+            case 1:
+                return "Both operands are must be true";
+            case 2:
+                return "One of the operands must be true";
+            case 3:
+                return "One of the operands must be true but not both";
+            case 4:
+                return "If the first operand is false, the second operand must be true | ~a v b";
+            case 5:
+                return "If the first operand is false, the second operand must be true and vice versa | (~a v b) ^ (a v ~b)";
+            default:
+                return "Invalid operator";
+        }
+    }
+}
---
+++ src/main/java/dev/arwa/App2.java
@@ -0,0 +1,123 @@
+package dev.arwa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Arwa
+ * @version 1.0
+ * @since 2023-03-23
+ *
+ */
+// This is a program that performs logical operations
+// and prints the result truth table in the console
+public class App2 {
+    final static String AND = "^";
+    final static String OR = "v";
+    final static String XOR = "*";
+    final static String IMPLICATION = ">";
+    final static String BICONDITIONAL = "=";
+    final static String Not = "~";
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Logical Operations Program");
+        System.out.println("Allowed variables are: Q, P, R, S");
+        System.out.println("Allowed operators are: ^, v, XOR, ->, <->, ~");
+        System.out.println("Example: ~P^Q->RvS");
+        System.out.println("========================================");
+        String expression = pickExpression();
+        System.out.println("You picked " + expression);
+        System.out.println("========================================");
+        System.out.println("Truth Table");
+        System.out.println("========================================");
+
+        List<String> variables = getVariables(expression);
+        List<Boolean> values = new ArrayList<>();
+        // System.out.println("P\tQ\tR\tS\t" + expression);
+        // System.out.println("========================================");
+        // System.out.println("F\tF\tF\tF\t" + performOperation(false, expression,
+        // false, false, false));
+    }
+
+    public static List<String> getVariables(String expression) {
+        List<String> variables = new ArrayList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == 'P' || expression.charAt(i) == 'Q' || expression.charAt(i) == 'R'
+                    || expression.charAt(i) == 'S') {
+                variables.add("" + expression.charAt(i));
+            }
+        }
+        return variables;
+    }
+
+    public boolean getValue(String complex, List<String> variables, List<Boolean> values) {
+        String operand = "" + complex.charAt(0);
+        if (operand.equals(Not)) {
+            LogicalOperand logicalOperand = new LogicalOperand("" + complex.charAt(1), true);
+        } else if (operand.equals(AND)) {
+            LogicalOperand logicalOperand = new LogicalOperand("" + complex.charAt(0), false);
+        }
+        for (int i = 0; i < variables.size(); i++) {
+            
+        }
+    }
+
+    public int getOperator(String opertor) {
+        switch (opertor) {
+            case AND:
+                return 1;
+            case OR:
+                return 2;
+            case XOR:
+                return 3;
+            case IMPLICATION:
+                return 4;
+            case BICONDITIONAL:
+                return 5;
+            default:
+                return 0;
+        }
+    }
+
+    public static String pickExpression() {
+        System.out.println("Enter an expression:");
+        Scanner scanner = new Scanner(System.in);
+        String res = scanner.nextLine();
+        res = res.toUpperCase();
+        res.replace("XOR", "*");
+        res.replace("->", ">");
+        res.replace("<->", "=");
+        res.replace("V", "v");
+        res.replace(" ", "");
+        scanner.close();
+        return res;
+    }
+
+}
+
+class LogicalOperand {
+    private String operand; // P, Q, R, S
+    private boolean isNegated; // ~
+    private boolean value; // true or false
+
+    public LogicalOperand(String operand, boolean isNegated) {
+        this.operand = operand;
+        this.isNegated = isNegated;
+    }
+
+    public void setValue(String operand, boolean newVal) {
+        if (operand.equals(this.operand)) {
+            if (isNegated) {
+                this.value = !newVal;
+            } else {
+                this.value = newVal;
+            }
+        }
+    }
+
+    public boolean getValue() {
+        return this.value;
+    }
+
+}
